678e5b9aa12aaccdc6d0f09e007f78ce00e043b3
JakWai01 <jakob.waibel@web.de>
2021-03-27 19:13:40 +0100

    feat: Connect database to use_case

--- consumatio/external/database.py
+++ consumatio/external/database.py
@@ -6,38 +6,41 @@ import uuid
 #cur = con.cursor()
 
 # Create Table (only first time)
-# cur.execute('''CREATE TABLE movies(code text, title text, year real, regisseur text, rating real)''')
+#cur.execute('''CREATE TABLE movies(code text, title text, year real, regisseur text, rating real)''')
 
 #con.commit()
 
 #con.close()
+class Database(): 
 
-def add_movie(dict):
-    con = sqlite3.connect('data.db')
-    cur = con.cursor()
+    def add_movie(self, dict):
+        con = sqlite3.connect('data.db')
+        cur = con.cursor()
 
-    code = dict.get('code')
-    title = dict.get('title')
-    year = dict.get('year')
-    regisseur = dict.get('regisseur')
-    rating = dict.get('rating')
+        cur.execute('''CREATE TABLE movies(code text, title text, year real, regisseur text, rating real)''')
+        
+        code = dict.get('code')
+        title = dict.get('title')
+        year = dict.get('year')
+        regisseur = dict.get('regisseur')
+        rating = dict.get('rating')
 
-    values = [(str(code), title, year, regisseur, rating)]
+        values = [(str(code), title, year, regisseur, rating)]
 
-    cur.executemany('INSERT INTO movies VALUES (?, ?, ?, ?, ?)', values)
-    
+        cur.executemany('INSERT INTO movies VALUES (?, ?, ?, ?, ?)', values)
+        
 
-    cur.execute('SELECT * FROM movies')
-    print(cur.fetchone())
-    
-    con.commit()
-    con.close()
+        cur.execute('SELECT * FROM movies')
+        print(cur.fetchone())
+        
+        con.commit()
+        con.close()
 
 
-add_movie({
-    "code": uuid.uuid4(),
-    "title": "The Matrix",
-    "year": 1999,
-    "regisseur": "The Wichowskis",
-    "rating": 8.7
-})
+#add_movie({
+#    "code": uuid.uuid4(),
+#    "title": "The Matrix",
+#    "year": 1999,
+#    "regisseur": "The Wichowskis",
+#    "rating": 8.7
+#})
--- consumatio/use_cases/list_movies.py
+++
@@ -1,2 +0,0 @@
-def list_movies(repo):
-    return repo.list()
