ba3d75ba6e1b7815f5acea3f6206cfadca8ffaa2
JakWai01 <jakob.waibel@web.de>
2021-04-11 22:57:24 +0200

    feat: Add tmdb skeleton

---
+++ consumatio/external/tmdb.py
@@ -0,0 +1,33 @@
+import requests
+
+class Tmdb():
+    
+    def get_movie_details(self):
+        pass
+
+    def get_movie_providers(self):
+        pass
+
+    def get_movie_images(self):
+        pass
+
+    def get_tv_details(self):
+        pass
+
+    def get_tv_providers(self):
+        pass
+
+    def get_tv_images(self):    
+        pass
+
+    def get_season_details(self):
+        pass
+
+    def get_season_images(self):
+        pass
+
+    def get_episode_details(self):
+        pass
+
+    def get_episode_images(self):
+        pass
