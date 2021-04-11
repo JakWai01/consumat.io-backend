6e155f0d4c100dd3f7441e2b04e801f8dfa854b6
dentremor <hillerdaniel11@gmail.com>
2021-04-11 19:10:28 +0200

    feat: Add test_episode_to_dict

--- tests/entities/test_episode.py
+++ tests/entities/test_episode.py
@@ -53,3 +53,22 @@ def test_movie_from_dict():
     assert episode.vote_average == 7.11904761904762
     assert episode.vote_count == 21
     assert episode.watch_status == "WATCHED"
+
+
+def test_episode_to_dict():
+    dict = {
+        "air_date": "2011-04-17",
+        "episode_number": 1,
+        "name": "Winter Is Coming",
+        "overview": "Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.",
+        "code": 63056,
+        "season_number": 1,
+        "still_path": "/wrGWeW4WKxnaeA8sxJb2T9O6ryo.jpg",
+        "vote_averag": 7.11904761904762,
+        "vote_count": 21,
+        "watch_status": "WATCHED"
+    }
+
+    episode = Episode.from_dict(dict)
+
+    assert episode.to_dict() == dict
