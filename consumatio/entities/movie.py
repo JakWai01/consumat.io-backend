ac94b30cb35dafcae7c5eb97c455df2945783110
JakWai01 <jakob.waibel@web.de>
2021-04-11 10:52:24 +0200

    feat: Add movie entity

---
+++ consumatio/entities/movie.py
@@ -0,0 +1,28 @@
+import dataclasses
+
+@dataclasses.dataclass
+class Movie():
+    id: int 
+    title: str
+    genres: dict
+    overview: str
+    popularity: int
+    vote_average: int
+    release_date: str
+    runtime: int
+    status: str
+    # Possibility to request different image sizes (not from api)
+    backdrop: dict
+    poster: dict
+    # Possibility to get providers according to provided country
+    providers: dict 
+    watch_status: str
+    rating: float
+    favorite: bool
+
+    @classmethod
+    def from_dict(self, dict):
+        return self(**dict)
+
+    def to_dict(self):
+        return dataclasses.asdict(self)
