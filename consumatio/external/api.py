e482c4df41eb32768877fbdb5a83b5ded0b3583d
JakWai01 <jakob.waibel@web.de>
2021-04-12 22:48:17 +0200

    feat: Add movie_details endpoint to graphql api

--- consumatio/external/api.py
+++ consumatio/external/api.py
@@ -1,16 +1,28 @@
-from graphene import ObjectType, String, Schema, Decimal
+from graphene import ObjectType, String, Schema, Decimal, Int, List, Field
 from flask import Flask, request
 import json
 from consumatio.external.tmdb import Tmdb
 
 app = Flask(__name__)
 
-class Query(ObjectType):
-    movieDetails = String(id=Decimal())
+class Movie(ObjectType):
+    code = Int()
+    title = String() 
+    genres = List(String)
+    overview = String()
+    popularity = Decimal()
+    vote_average = Decimal()
+    release_date = String()
+    runtime = Int()
+    status = String()
 
-    def resolve_movieDetails(root, info, id):
+class Query(ObjectType):
+    movie_details = Field(Movie, id=Decimal())
+
+    def resolve_movie_details(root, info, id):
         tmdb = Tmdb()
-        return tmdb.get_movie_details(id)
+        dict = tmdb.get_movie_details(id)
+        return Movie(code=dict.get("code"), title=dict.get("title"), genres=dict.get("genres"), overview=dict.get("overview"), popularity=dict.get("popularity"), vote_average=dict.get("vote_average"), release_date=dict.get("release_date"), runtime=dict.get("runtime"), status=dict.get("status"))
 
 schema = Schema(query=Query)
 
