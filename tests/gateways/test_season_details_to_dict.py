2d8d381ea1430c78a7224b6e19f93668d1cdf3cb
JakWai01 <jakob.waibel@web.de>
2021-04-14 12:39:16 +0200

    feat: Add season gateway tests

---
+++ tests/gateways/test_season_details_to_dict.py
@@ -0,0 +1,22 @@
+from consumatio.gateways.season_details_to_dict import *
+
+def test_season_details_to_dict():
+    json = {
+        "_id": "5256c89f19c2956ff6046d47",
+        "air_date": "2011-04-17",
+        "name": "Season 1",
+        "overview": "Trouble is brewing in the Seven Kingdoms of Westeros. For the driven inhabitants of this visionary world, control of Westeros' Iron Throne holds the lure of great power. But in a land where the seasons can last a lifetime, winter is coming...and beyond the Great Wall that protects them, an ancient evil has returned. In Season One, the story centers on three primary areas: the Stark and the Lannister families, whose designs on controlling the throne threaten a tenuous peace; the dragon princess Daenerys, heir to the former dynasty, who waits just over the Narrow Sea with her malevolent brother Viserys; and the Great Wall--a massive barrier of ice where a forgotten danger is stirring.",
+        "id": 3624,
+        "poster_path": "/zwaj4egrhnXOBIit1tyb4Sbt3KP.jpg",
+        "season_number": 1
+    }
+
+    dict = {
+        'code': 3624,
+        'tv_code': 1399, 
+        'season_number': 1, 
+        'name': 'Season 1', 
+        'overview': "Trouble is brewing in the Seven Kingdoms of Westeros. For the driven inhabitants of this visionary world, control of Westeros' Iron Throne holds the lure of great power. But in a land where the seasons can last a lifetime, winter is coming...and beyond the Great Wall that protects them, an ancient evil has returned. In Season One, the story centers on three primary areas: the Stark and the Lannister families, whose designs on controlling the throne threaten a tenuous peace; the dragon princess Daenerys, heir to the former dynasty, who waits just over the Narrow Sea with her malevolent brother Viserys; and the Great Wall--a massive barrier of ice where a forgotten danger is stirring."
+    }
+
+    assert season_details_to_dict(json, 1399) == dict
---
+++ tests/gateways/test_season_images_to_dict.py
@@ -0,0 +1,22 @@
+from consumatio.gateways.season_images_to_dict import *
+
+def test_season_images_to_dict():
+    json = {
+        "id": 3624,
+        "posters": [
+            {
+                "aspect_ratio": 0.666666666666667,
+                "file_path": "/olJ6ivXxCMq3cfujo1IRw30OrsQ.jpg",
+                "height": 1425,
+                "iso_639_1": "en",
+                "vote_average": 5.37612146307798,
+                "vote_count": 6,
+                "width": 950
+            }
+        ]
+    }
+    print(season_images_to_dict(json))
+    dict = {'posters': [{'aspect_ratio': 0.666666666666667, 'file_path': '/olJ6ivXxCMq3cfujo1IRw30OrsQ.jpg',
+                         'height': 1425, 'iso_639_1': 'en', 'vote_average': 5.37612146307798, 'vote_count': 6, 'width': 950}]}
+
+    assert season_images_to_dict(json) == dict
