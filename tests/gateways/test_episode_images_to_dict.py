dec04e881b2a54973c6e62c11198f720019a185c
JakWai01 <jakob.waibel@web.de>
2021-04-15 16:21:21 +0200

    fix: Make tests run

--- tests/gateways/test_episode_images_to_dict.py
+++ tests/gateways/test_episode_images_to_dict.py
@@ -17,10 +17,10 @@ def test_episode_images_to_dict():
     }
 
     dict = {
-        'stills': [
+        'stills': str([
             {'aspect_ratio': 1.77777777777778, 'file_path': '/wrGWeW4WKxnaeA8sxJb2T9O6ryo.jpg', 'height': 1080,
                 'iso_639_1': None, 'vote_average': 5.30505952380952, 'vote_count': 1, 'width': 1920}
-        ]
+        ])
     }
 
     assert episode_images_to_dict(json) == dict
--- tests/gateways/test_movie_images_to_dict.py
+++ tests/gateways/test_movie_images_to_dict.py
@@ -29,12 +29,12 @@ def test_movie_images_to_dict():
 }
 
     dict = {
-        'backdrops': [
+        'backdrops': str([
             {'aspect_ratio': 1.77777777777778, 'file_path': '/fCayJrkfRaCRCTh8GqN30f8oyQF.jpg', 'height': 720, 'iso_639_1': None, 'vote_average': 0, 'vote_count': 0, 'width': 1280}
-        ],
-        'posters': [
+        ]),
+        'posters': str([
             {'aspect_ratio': 0.666666666666667, 'file_path': '/fpemzjF623QVTe98pCVlwwtFC5N.jpg', 'height': 1800, 'iso_639_1': 'en', 'vote_average': 0, 'vote_count': 0, 'width': 1200}
-        ]
+        ])
     }
 
     assert movie_images_to_dict(json) == dict
