352af69f9937f5318ae84360184a10c24ef836a2
JakWai01 <jakob.waibel@web.de>
2021-04-17 23:39:39 +0200

    feat: Add caching to database

--- consumatio/external/tmdb.py
+++ consumatio/external/tmdb.py
@@ -13,43 +13,53 @@ from consumatio.gateways.episode_images_to_dict import *
 class Tmdb():
     
     def get_movie_details(self, movie_id):
-        data = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=c4bd02adee6e73c4d17e69b039267c90&language=en-US').json()
+        query = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=c4bd02adee6e73c4d17e69b039267c90&language=en-US'
+        data = requests.get(query).json()
         return movie_details_to_dict(data)
 
     def get_movie_providers(self, movie_id, country):
-        data = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers?api_key=c4bd02adee6e73c4d17e69b039267c90'
+        data = requests.get(query).json()
         data = data['results'][country]
         return movie_providers_to_dict(data)
 
     def get_movie_images(self, movie_id):
-        data = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=c4bd02adee6e73c4d17e69b039267c90'
+        data = requests.get(query).json()
         return movie_images_to_dict(data)
 
     def get_tv_details(self, tv_id):
-        data = requests.get(f'https://api.themoviedb.org/3/tv/{tv_id}?api_key=c4bd02adee6e73c4d17e69b039267c90&language=en-US').json()
+        query = f'https://api.themoviedb.org/3/tv/{tv_id}?api_key=c4bd02adee6e73c4d17e69b039267c90&language=en-US'
+        data = requests.get(query).json()
         return tv_details_to_dict(data)
 
     def get_tv_providers(self, tv_id, country):
-        data = requests.get(f'https://api.themoviedb.org/3/tv/{tv_id}/watch/providers?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/tv/{tv_id}/watch/providers?api_key=c4bd02adee6e73c4d17e69b039267c90' 
+        data = requests.get(query).json()
         data = data['results'][country]
         return tv_providers_to_dict(data)
 
     def get_tv_images(self, tv_id):    
-        data = requests.get(f'https://api.themoviedb.org/3/tv/{tv_id}/images?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/tv/{tv_id}/images?api_key=c4bd02adee6e73c4d17e69b039267c90'
+        data = requests.get(query).json()
         return tv_images_to_dict(data)
 
     def get_season_details(self, tv_id, season_number):
-        data = requests.get(f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}?api_key=c4bd02adee6e73c4d17e69b039267c90'
+        data = requests.get(query).json()
         return season_details_to_dict(data, tv_id)
 
     def get_season_images(self, tv_id, season_number):
-        data = requests.get(f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/images?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/images?api_key=c4bd02adee6e73c4d17e69b039267c90' 
+        data = requests.get(query).json()
         return season_images_to_dict(data)
 
     def get_episode_details(self, tv_id, season_number, episode_number):
-        data = requests.get(f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/episode/{episode_number}?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/episode/{episode_number}?api_key=c4bd02adee6e73c4d17e69b039267c90'
+        data = requests.get(query).json()
         return episode_details_to_dict(data)
 
     def get_episode_images(self, tv_id, season_number, episode_number):
-        data = requests.get(f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/episode/{episode_number}/images?api_key=c4bd02adee6e73c4d17e69b039267c90').json()
+        query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/episode/{episode_number}/images?api_key=c4bd02adee6e73c4d17e69b039267c90'
+        data = requests.get(query).json()
         return episode_images_to_dict(data)
