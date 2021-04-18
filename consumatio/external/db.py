4f5ed760e1745eacd150d1fb33689ee56fcb87a8
JakWai01 <jakob.waibel@web.de>
2021-04-18 12:09:10 +0200

    feat: Move caching to class and convert cached strings into dictionaries

--- consumatio/external/db.py
+++ consumatio/external/db.py
@@ -1,43 +1,44 @@
 import sqlite3
 import datetime
 
-def cache(query, body):
-    con = sqlite3.connect('db.sqlite3')
-    cur = con.cursor()
+class Database():
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS cache(query text UNIQUE, body text, last_changed date)''')
+    def cache(self, query, body):
+        con = sqlite3.connect('db.sqlite3')
+        cur = con.cursor()
 
-    cur.execute('''INSERT INTO CACHE VALUES (:query, :body, :last_changed)''', {"query": query, "body": body, "last_changed": datetime.date.today()})
+        cur.execute('''CREATE TABLE IF NOT EXISTS cache(query text UNIQUE, body text, last_changed date)''')
 
-    con.commit()
-    con.close()
+        cur.execute('''INSERT INTO CACHE VALUES (:query, :body, :last_changed)''', {"query": query, "body": body, "last_changed": datetime.date.today()})
 
-def is_cached(query):
-    con = sqlite3.connect('db.sqlite3')
-    cur = con.cursor()
-
-    cur.execute('''CREATE TABLE IF NOT EXISTS cache(query text UNIQUE, body text, last_changed date)''')
-
-    cur.execute('SELECT * FROM cache WHERE query=:query', {"query": query})
-
-    result = cur.fetchall()
-    print(result)
-
-    if len(result) != 0 and datetime.date.today() - datetime.timedelta(days = 10) < datetime.datetime.strptime(result[0][2], '%Y-%m-%d').date():
         con.commit()
         con.close()
-        return True
-    else:
-        con.commit()
-        con.close()
-        return False
 
-def get_from_cache(query):
-    con = sqlite3.connect('db.sqlite3')
-    cur = con.cursor()
+    def is_cached(self, query):
+        con = sqlite3.connect('db.sqlite3')
+        cur = con.cursor()
 
-    cur.execute('SELECT body from cache WHERE query=:query', {"query": query})
+        cur.execute('''CREATE TABLE IF NOT EXISTS cache(query text UNIQUE, body text, last_changed date)''')
 
-    result = cur.fetchall()
+        cur.execute('SELECT * FROM cache WHERE query=:query', {"query": query})
 
-    return result[0][0]
+        result = cur.fetchall()
+
+        if len(result) != 0 and datetime.date.today() - datetime.timedelta(days = 10) < datetime.datetime.strptime(result[0][2], '%Y-%m-%d').date():
+            con.commit()
+            con.close()
+            return True
+        else:
+            con.commit()
+            con.close()
+            return False
+
+    def get_from_cache(self, query):
+        con = sqlite3.connect('db.sqlite3')
+        cur = con.cursor()
+
+        cur.execute('SELECT body from cache WHERE query=:query', {"query": query})
+
+        result = cur.fetchall()
+
+        return result[0][0]
