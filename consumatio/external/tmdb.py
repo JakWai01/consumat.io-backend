4585a153ff086747023880117027f20b74ba2a09
JakWai01 <jakob.waibel@web.de>
2021-04-18 12:09:10 +0200

    feat: Move caching to class and convert cached strings into dictionaries

--- consumatio/external/tmdb.py
+++ consumatio/external/tmdb.py
@@ -9,101 +9,69 @@ from consumatio.gateways.season_details_to_dict import *
 from consumatio.gateways.season_images_to_dict import * 
 from consumatio.gateways.episode_details_to_dict import * 
 from consumatio.gateways.episode_images_to_dict import *
-from consumatio.external.db import cache
-from consumatio.external.db import is_cached
-from consumatio.external.db import get_from_cache
+from consumatio.external.db import Database
 
 class Tmdb():
     
     def get_movie_details(self, movie_id):
         query = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=c4bd02adee6e73c4d17e69b039267c90&language=en-US'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data))
+        data = get_data(query)
         return movie_details_to_dict(data)
 
     def get_movie_providers(self, movie_id, country):
         query = f'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers?api_key=c4bd02adee6e73c4d17e69b039267c90'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data))
+        data = get_data(query)
         data = data['results'][country]
         return movie_providers_to_dict(data)
 
     def get_movie_images(self, movie_id):
         query = f'https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=c4bd02adee6e73c4d17e69b039267c90'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data))
+        data = get_data(query)
         return movie_images_to_dict(data)
 
     def get_tv_details(self, tv_id):
         query = f'https://api.themoviedb.org/3/tv/{tv_id}?api_key=c4bd02adee6e73c4d17e69b039267c90&language=en-US'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data)) 
+        data = get_data(query)
         return tv_details_to_dict(data)
 
     def get_tv_providers(self, tv_id, country):
         query = f'https://api.themoviedb.org/3/tv/{tv_id}/watch/providers?api_key=c4bd02adee6e73c4d17e69b039267c90' 
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data)) 
+        data = get_data(query)
         data = data['results'][country]
         return tv_providers_to_dict(data)
 
     def get_tv_images(self, tv_id):    
         query = f'https://api.themoviedb.org/3/tv/{tv_id}/images?api_key=c4bd02adee6e73c4d17e69b039267c90'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data)) 
+        data = get_data(query)
         return tv_images_to_dict(data)
 
     def get_season_details(self, tv_id, season_number):
         query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}?api_key=c4bd02adee6e73c4d17e69b039267c90'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data)) 
+        data = get_data(query)
         return season_details_to_dict(data, tv_id)
 
     def get_season_images(self, tv_id, season_number):
         query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/images?api_key=c4bd02adee6e73c4d17e69b039267c90' 
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data))
+        data = get_data(query)
         return season_images_to_dict(data)
 
     def get_episode_details(self, tv_id, season_number, episode_number):
         query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/episode/{episode_number}?api_key=c4bd02adee6e73c4d17e69b039267c90'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data)) 
+        data = get_data(query)
         return episode_details_to_dict(data)
 
     def get_episode_images(self, tv_id, season_number, episode_number):
         query = f'https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}/episode/{episode_number}/images?api_key=c4bd02adee6e73c4d17e69b039267c90'
-        if (is_cached(query)):
-           data = get_from_cache(query) 
-        else:
-           data = requests.get(query).json() 
-           cache(query, str(data))
+        data = get_data(query)
         return episode_images_to_dict(data)
-        
+
+def get_data(query):
+   db = Database()
+
+   if (db.is_cached(query)):
+      data = eval(db.get_from_cache(query))
+   else:
+      data = requests.get(query).json() 
+      db.cache(query, str(data))
+
+   return data
