a964ec36388185fd22e4fc633382f511c4a4af95
JakWai01 <jakob.waibel@web.de>
2021-04-18 19:39:53 +0200

    fix: Effort to implement requested changes

--- consumatio/external/db.py
+++ consumatio/external/db.py
@@ -2,46 +2,31 @@ import sqlite3
 import datetime
 
 class Database():
+    def __init__(self):
+        self.con = sqlite3.connect('db.sqlite3')
+        self.cur = self.con.cursor()
+
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS cache(query text UNIQUE, body text, last_changed date)''')
 
     def cache(self, query, body):
-        con = sqlite3.connect('db.sqlite3')
-        cur = con.cursor()
-
-        cur.execute('''CREATE TABLE IF NOT EXISTS cache(query text UNIQUE, body text, last_changed date)''')
-
-        cur.execute('''INSERT INTO CACHE VALUES (:query, :body, :last_changed)''', {"query": query, "body": body, "last_changed": datetime.date.today()})
-
-        con.commit()
-        con.close()
+        self.cur.execute('''INSERT INTO CACHE VALUES (:query, :body, :last_changed)''', {"query": query, "body": body, "last_changed": datetime.date.today()})
 
     def is_cached(self, query):
-        con = sqlite3.connect('db.sqlite3')
-        cur = con.cursor()
+        self.cur.execute('SELECT * FROM cache WHERE query=:query', {"query": query})
 
-        cur.execute('''CREATE TABLE IF NOT EXISTS cache(query text UNIQUE, body text, last_changed date)''')
-
-        cur.execute('SELECT * FROM cache WHERE query=:query', {"query": query})
-
-        result = cur.fetchall()
+        result = self.cur.fetchall()
 
         if len(result) != 0 and datetime.date.today() - datetime.timedelta(days = 10) < datetime.datetime.strptime(result[0][2], '%Y-%m-%d').date():
-            con.commit()
-            con.close()
             return True
         else:
             if len(result) != 0 and datetime.date.today() - datetime.timedelta(days = 10) > datetime.datetime.strptime(result[0][2], '%Y-%m-%d').date():
-                cur.execute('''DELETE FROM cache WHERE query=:query''', {"query": query})
+                self.cur.execute('''DELETE FROM cache WHERE query=:query''', {"query": query})
 
-            con.commit()
-            con.close()
             return False
 
     def get_from_cache(self, query):
-        con = sqlite3.connect('db.sqlite3')
-        cur = con.cursor()
+        self.cur.execute('SELECT body from cache WHERE query=:query', {"query": query})
 
-        cur.execute('SELECT body from cache WHERE query=:query', {"query": query})
-
-        result = cur.fetchall()
+        result = self.cur.fetchall()
 
         return result[0][0]
