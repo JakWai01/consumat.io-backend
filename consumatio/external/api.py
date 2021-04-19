42ec20cb6bdd26d72f67072524464e2e3d8d72b0
dentremor <hillerdaniel11@gmail.com>
2021-04-19 21:49:19 +0200

    feat: Add multisearch to api

--- consumatio/external/api.py
+++ consumatio/external/api.py
@@ -1,12 +1,17 @@
 from ariadne import ObjectType, QueryType, gql, make_executable_schema, graphql_sync
 from consumatio.external.tmdb import Tmdb
-from consumatio.usecases.movie_details import * 
+from consumatio.usecases.movie_details import *
 from consumatio.usecases.tv_details import *
 from consumatio.usecases.season_details import *
 from consumatio.usecases.episode_details import *
+from consumatio.usecases.search_details import *
 from flask import Flask, request, jsonify
 from ariadne.constants import PLAYGROUND_HTML
 import os
+
+
+# TODO @Danny: Fix the error which is probably caused because we get
+#              a list with objects
 
 type_defs = gql("""
     type Query {
@@ -14,6 +19,11 @@ type_defs = gql("""
         tv(code: Int, country: String): TV!
         season(code: Int, seasonNumber: Int): Season!
         episode(code: Int, seasonNumber: Int, episodeNumber: Int): Episode!
+        search(str: String): Search!
+    }
+    
+    type Search{
+        results: [Movie]
     }
 
     type Movie {
@@ -83,39 +93,61 @@ app = Flask(__name__)
 
 query = QueryType()
 
+
 @query.field("movie")
 def resolve_movie(*_, code, country):
     tmdb = Tmdb()
-    return  movie_details(tmdb, code, country)
+    return movie_details(tmdb, code, country)
+
 
 movie = ObjectType("Movie")
+
 
 @query.field("tv")
 def resolve_tv(*_, code, country):
     tmdb = Tmdb()
     return tv_details(tmdb, code, country)
 
+
 tv = ObjectType("TV")
+
 
 @query.field("season")
 def resolve_season(*_, code, seasonNumber):
     tmdb = Tmdb()
     return season_details(tmdb, code, seasonNumber)
 
+
 season = ObjectType("Season")
+
 
 @query.field("episode")
 def resolve_episode(*_, code, seasonNumber, episodeNumber):
     tmdb = Tmdb()
     return episode_details(tmdb, code, seasonNumber, episodeNumber)
 
+
 episode = ObjectType("Episode")
 
-schema = make_executable_schema(type_defs, query, movie, tv, season)
+
+@query.field("search")
+# TODO @Danny: Implement minLength: 2
+#                         pattern:([a-z]{2})-([A-Z]{2})
+def resolve_search(*_, str):
+    tmdb = Tmdb()
+    return search_details(tmdb, str)
+
+
+search = ObjectType("Search")
+
+
+schema = make_executable_schema(type_defs, query, movie, tv, season, search)
+
 
 @app.route("/graphql", methods=["GET"])
 def graphql_playground():
     return PLAYGROUND_HTML, 200
+
 
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
@@ -130,6 +162,7 @@ def graphql_server():
 
     status_code = 200 if success else 400
     return jsonify(result), status_code
+
 
 port = int(os.environ['PORT'])
 
