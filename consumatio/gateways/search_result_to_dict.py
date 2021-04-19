2e14b5e741d51c86c3b58e4964016abfc802fc7d
dentremor <hillerdaniel11@gmail.com>
2021-04-19 22:50:39 +0200

    feat: Make the gateway work with minimal requirements

--- consumatio/gateways/search_result_to_dict.py
+++ consumatio/gateways/search_result_to_dict.py
@@ -2,48 +2,32 @@
 def search_result_to_dict(data):
 
     # TODO @Danny: check if enumeration of api-data works
-    array = []
-    for idx, element in enumerate(data):
-        if idx == 0:
-            continue
-        for values in element:
-            if values.media_type == "movie":
-                dict = {
-                    "media_type": data["media_type"],
-                    "code": data["id"],
-                    "title": data["title"],
-                    "genres": data["genres"],
-                    "overview": data["overview"],
-                    "popularity": data["popularity"],
-                    "voteAverage": data["vote_average"],
-                    "releaseDate": data["release_date"],
-                    "runtime": data["runtime"],
-                    "status": data["status"],
-                    "backdrops": data["backdrops"],
-                    "posters": data["posters"],
-                    "providers": data["providers"],
-                    "watchStatus": None,
-                    "rating": None,
-                    "favorite": None
-                }
+    results = data["results"]
+    result_list = []
+    results_filtered = []
 
-            elif values.media_type == "movie":
-                dict = {
-                    "code": data["id"],
-                    "name": data["name"],
-                    "genres": data["genres"],
-                    "overview": data["overview"],
-                    "popularity": data["popularity"],
-                    "voteAverage": data["vote_average"],
-                    "firstAirDate": data["first_air_date"],
-                    "lastAirDate": data["last_air_date"],
-                    "status": data["status"],
-                    "backdrops": data["backdrops"],
-                    "posters": data["posters"],
-                    "providers": data["providers"],
-                    "watchStatus": None,
-                    "rating": None,
-                    "favorite": None
-                }
-            array.append(dict)
-    return array
+    for result in results:
+        if result.get("media_type") == "tv" or result.get("media_type") == "movie":
+            results_filtered.append(result)
+
+    for result in results_filtered:
+        cache = []
+        cache.append(result.get("id"))
+        cache.append(result.get("media_type"))
+        if result.get("media_type") == "tv":
+            cache.append(result.get("name"))
+        else:
+            cache.append(result.get("titel"))
+        cache.append(result.get("overview"))
+        if result.get("media_type") == "tv":
+            cache.append(result.get("first_air_date"))
+        else:
+            cache.append(result.get("release_date"))
+        cache.append(result.get("poster_path"))
+        if result.get("media_type") == "tv":
+            cache.append("Number of Seasons")
+        cache.append("Watchstatus")
+
+        result_list.append(cache)
+
+    return result_list
