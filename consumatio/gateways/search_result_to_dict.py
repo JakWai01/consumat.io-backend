a24cc7c74eca6c61d5ea2a8859be26bf899f4811
dentremor <hillerdaniel11@gmail.com>
2021-04-20 22:51:33 +0200

    feat: Improve the search-function

--- consumatio/gateways/search_result_to_dict.py
+++ consumatio/gateways/search_result_to_dict.py
@@ -1,33 +1,59 @@
+import consumatio.external.api as Api
+
 
 def search_result_to_dict(data):
 
     # TODO @Danny: check if enumeration of api-data works
     results = data["results"]
     result_list = []
-    results_filtered = []
+    # results_filtered = []
 
     for result in results:
-        if result.get("media_type") == "tv" or result.get("media_type") == "movie":
-            results_filtered.append(result)
-
-    for result in results_filtered:
-        cache = []
-        cache.append(result.get("id"))
-        cache.append(result.get("media_type"))
         if result.get("media_type") == "tv":
-            cache.append(result.get("name"))
-        else:
-            cache.append(result.get("titel"))
-        cache.append(result.get("overview"))
-        if result.get("media_type") == "tv":
-            cache.append(result.get("first_air_date"))
-        else:
-            cache.append(result.get("release_date"))
-        cache.append(result.get("poster_path"))
-        if result.get("media_type") == "tv":
-            cache.append("Number of Seasons")
-        cache.append("Watchstatus")
-
-        result_list.append(cache)
-
+            tmp = Api.resolve_tv(code=result.get("id"), country="DE")
+            dict = {
+                "code": result.get("id"),
+                "media_type": result.get("media_type"),
+                "title": result.get("name"),
+                "overview": result.get("overview"),
+                "release_date": result.get("first_air_date"),
+                "poster_path": result.get("poster_path"),
+                "season_count": tmp.get("season_number"),
+                "watch_status": None
+            }
+        elif result.get("media_type") == "movie":
+            dict = {
+                "code": result.get("id"),
+                "media_type": result.get("media_type"),
+                "title": result.get("title"),
+                "overview": result.get("overview"),
+                "release_date": result.get("release_date"),
+                "poster_path": result.get("poster_path"),
+                "season_count": None,
+                "watch_status": None
+            }
+        result_list.append(dict)
     return result_list
+
+    # for result in results_filtered:
+    #     cache = []
+    #     cache.append(result.get("id"))
+    #     cache.append(result.get("media_type"))
+    #     if result.get("media_type") == "tv":
+    #         cache.append(result.get("name"))
+    #     else:
+    #         cache.append(result.get("titel"))
+    #     cache.append(result.get("overview"))
+    #     if result.get("media_type") == "tv":
+    #         cache.append(result.get("first_air_date"))
+    #     else:
+    #         cache.append(result.get("release_date"))
+    #     cache.append(result.get("poster_path"))
+    #     if result.get("media_type") == "tv":
+    #         # cache.append(-1)
+    #         # must be implemented with another api request
+    #         tmp = Api.resolve_tv(code=result.get("id"), country="DE")
+    #         cache.append(tmp.get("season_number"))
+    #     result_list.append(cache)
+
+    # return result_list
--- consumatio/usecases/search_details.py
+++ consumatio/usecases/search_details.py
@@ -1,14 +1,16 @@
 
 def search_details(tmdb, str):
-    dict_search_details = tmdb.get_search_details(str)
+    dict_search_details = tmdb.get_search(str)
 
-    # "media_type": dict_search_details.get("media_type"),
-    # "code": dict_search_details.get("code"),
-    # "title": dict_search_details.get("title"),
-    # "overview": dict_search_details.get("overview"),
-    # "releaseDate": dict_search_details.get("release_date"),
-    # "posters": dict_search_details.get("posters"),
-    # "numberOfSeasons": dict_search_details.get(),
-    # "watchStatus": None
+    dict = {
+        "code": dict_search_details.get("code"),
+        "mediaType": dict_search_details.get("media_type"),
+        "title": dict_search_details.get("title"),
+        "overview": dict_search_details.get("overview"),
+        "releaseDate": dict_search_details.get("release_date"),
+        "posterPath": dict_search_details.get("poster_path"),
+        "seasonCount": dict_search_details.get("season_count"),
+        "watchStatus": dict_search_details.get("watch_status")
+    }
 
     return dict_search_details
