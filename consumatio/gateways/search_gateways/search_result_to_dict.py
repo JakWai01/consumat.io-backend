2822dcf252453a91cecbc434f2b72202eb1dda41
JakWai01 <jakob.waibel@web.de>
2021-05-05 16:01:51 +0200

    feat: Add UnionType for search

--- consumatio/gateways/search_gateways/search_result_to_dict.py
+++ consumatio/gateways/search_gateways/search_result_to_dict.py
@@ -1,3 +1,7 @@
+from consumatio.entities.movie import Movie
+from consumatio.entities.tv import TV
+
+
 def search_result_to_dict(data: dict) -> dict:
     """
     Create dictionary for internal representation
@@ -16,23 +20,61 @@ def search_result_to_dict(data: dict) -> dict:
             if result.get("media_type") == "tv":
                 dict = {
                     "code": result.get("id"),
-                    "media_type": result.get("media_type"),
                     "title": result.get("name"),
+                    "genres": None,
                     "overview": result.get("overview"),
-                    "release_date": result.get("first_air_date"),
+                    "popularity": result.get("popularity"),
+                    "rating_average": result.get("vote_average"),
+                    "first_air_date": result.get("first_air_date"),
+                    "last_air_date": None,
+                    "status": None,
+                    "backdrop_path": result.get("backdrop_path"),
                     "poster_path": result.get("poster_path"),
-                    "watch_status": None
+                    "providers": None,
+                    "creators": None,
+                    "cast": None,
+                    "number_of_episodes": None,
+                    "number_of_seasons": None,
+                    "tmdb_url":
+                    f'https://www.themoviedb.org/tv/{result.get("id")}',
+                    "watch_status": None,
+                    "rating_user": None,
+                    "favorite": None
                 }
+
+                tv = TV.from_dict(dict)
+
+                dict = tv.to_dict()
+
+                dict["__typename"] = "TV"
             elif result.get("media_type") == "movie":
                 dict = {
                     "code": result.get("id"),
-                    "media_type": result.get("media_type"),
                     "title": result.get("title"),
+                    "genres": None,
                     "overview": result.get("overview"),
+                    "popularity": result.get("popularity"),
+                    "rating_average": result.get("vote_average"),
                     "release_date": result.get("release_date"),
+                    "runtime": None,
+                    "status": None,
+                    "backdrop_path": result.get("backdrop_path"),
                     "poster_path": result.get("poster_path"),
-                    "watch_status": None
+                    "providers": None,
+                    "cast": None,
+                    "directors": None,
+                    "tmdb_url":
+                    f'https://www.themoviedb.org/movie/{result.get("id")}',
+                    "watch_status": None,
+                    "rating_user": None,
+                    "favorite": None
                 }
+
+                movie = Movie.from_dict(dict)
+
+                dict = movie.to_dict()
+
+                dict["__typename"] = "Movie"
             else:
                 continue
             result_list.append(dict)
