04a6a3981f0078a48e566d4ae15e7c4309824393
dentremor <hillerdaniel11@gmail.com>
2021-05-06 17:22:18 +0200

    add: Create a file to handle the logger

---
+++ consumatio/external/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def get_logger_instance() -> object:
+    """
+    Configures the logger for the backend an returns a instance
+    :return: <object> Logger instance
+    """
+    logging.basicConfig(level=logging.INFO & logging.DEBUG,
+                        format='%(asctime)s : %(levelname)s : %(message)s')
+    return logging.getLogger(__name__)
