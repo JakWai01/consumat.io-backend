7028cdb45892eb7f82f4e99459fe3ea6e59a0739
dentremor <hillerdaniel11@gmail.com>
2021-05-18 14:53:13 +0200

    feat: Add page to search_deatils

--- consumatio/usecases/search_details.py
+++ consumatio/usecases/search_details.py
@@ -1,11 +1,12 @@
 class SearchDetails:
-    def get_search_details(self: object, user: str, tmdb: object,
-                           keyword: str) -> dict:
+    def get_search_details(self: object, user: str, tmdb: object, keyword: str,
+                           page: int) -> dict:
         """
         Make all relevant API request for this usecase (search) and assemble them into a dictionary
         :param tmdb: <object> Tmdb object
         :param keyword: <str> Search string
+        :param page: <int> Search page (minimum:1 maximum:1000)
         :return: <dict> Search details
         """
-        dict_search_details = tmdb.get_search(user, keyword)
+        dict_search_details = tmdb.get_search(user, keyword, page)
         return dict_search_details
