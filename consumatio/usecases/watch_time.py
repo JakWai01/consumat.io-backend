fd6217287d793acf6978fbad4d785326a5c6f5cc
JakWai01 <jakob.waibel@web.de>
2021-05-19 13:27:24 +0200

    fix: Bug on watchTime query

--- consumatio/usecases/watch_time.py
+++ consumatio/usecases/watch_time.py
@@ -7,7 +7,7 @@ class WatchTime:
     def get_watch_time(self: object, tmdb: object, user: str,
                        type: str) -> int:
         runtime = 0
-        if type == "Movie":
+        if type == 'Movie':
             results = MediaData.query.from_statement(
                 text(
                     "SELECT * FROM media_data, user_data WHERE user_data.user_id_content = media_data.user_id_content_media_data AND media_data.media_type_content = :type AND user_data.external_id_content = :user AND media_data.watch_status_content = 'Finished';"
@@ -17,8 +17,7 @@ class WatchTime:
                 data = tmdb.get_movie_details(result.media_id_content)
 
                 runtime += data.get("runtime")
-
-        if type == "TV":
+        elif type == 'TV':
             results = MediaData.query.from_statement(
                 text(
                     "SELECT * FROM media_data, user_data WHERE user_data.user_id_content = media_data.user_id_content_media_data AND media_data.media_type_content = 'Episode' AND user_data.external_id_content = :user AND media_data.watch_status_content = 'Finished';"
