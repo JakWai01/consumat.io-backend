fa7667d6a1399de10bdcc36eb8ce00969e55d4bd
JakWai01 <jakob.waibel@web.de>
2021-05-19 20:20:55 +0200

    feat: Add rating for episodes

--- consumatio/usecases/tv_episode_details.py
+++ consumatio/usecases/tv_episode_details.py
@@ -5,7 +5,7 @@ class TVEpisodeDetails:
         number_of_episodes = season_details.get("number_of_episodes")
 
         result = []
-        for i in range(1, number_of_episodes):
+        for i in range(1, number_of_episodes + 1):
             episode = tmdb.get_episode_details(code, season_number, i)
             result.append(episode)
 
