3859b1b4e7aa547a254a57da4ce89cfc1a5f3675
JakWai01 <jakob.waibel@web.de>
2021-05-24 11:02:10 +0200

    feat: Remove unnecessary classes

--- consumatio/usecases/number_of_watched_episodes.py
+++ consumatio/usecases/number_of_watched_episodes.py
@@ -1,17 +1,16 @@
-class NumberOfWatchedEpisodes:
-    def number_of_watched_episodes(self, tmdb, database, external_id, code,
+def set_number_of_watched_episodes(tmdb, database, external_id, code,
                                    seasonNumber, numberOfWatchedEpisodes):
-        user_id = 0
+    user_id = 0
 
-        if not database.user_exists(external_id):
-            database.user(external_id)
-        user_id = database.get_user_id(external_id)
+    if not database.user_exists(external_id):
+        database.user(external_id)
+    user_id = database.get_user_id(external_id)
 
-        data = tmdb.get_season_details(code, seasonNumber)
+    data = tmdb.get_season_details(code, seasonNumber)
 
-        if not database.media_data_exists(user_id, "Season", data.get("code")):
-            database.media_Data(user_id, "Season", data.get("code"))
+    if not database.media_data_exists(user_id, "Season", data.get("code")):
+        database.media_Data(user_id, "Season", data.get("code"))
 
-        database.number_of_watched_episodes(user_id, data.get("code"),
-                                            numberOfWatchedEpisodes)
-        return {"status": True}
+    database.number_of_watched_episodes(user_id, data.get("code"),
+                                        numberOfWatchedEpisodes)
+    return {"status": True}
--- consumatio/usecases/watch_status.py
+++ consumatio/usecases/watch_status.py
@@ -1,14 +1,12 @@
-class WatchStatus:
-    def watch_status(self, tmdb, database, external_id, code, media,
-                     watchStatus):
-        user_id = 0
+def set_watch_status(database, external_id, code, media, watchStatus):
+    user_id = 0
 
-        if not database.user_exists(external_id):
-            database.user(external_id)
-        user_id = database.get_user_id(external_id)
+    if not database.user_exists(external_id):
+        database.user(external_id)
+    user_id = database.get_user_id(external_id)
 
-        if not database.media_data_exists(user_id, media, code):
-            database.media_Data(user_id, media, code)
+    if not database.media_data_exists(user_id, media, code):
+        database.media_Data(user_id, media, code)
 
-        database.watch_status(user_id, media, code, watchStatus)
-        return {"status": True}
+    database.watch_status(user_id, media, code, watchStatus)
+    return {"status": True}
