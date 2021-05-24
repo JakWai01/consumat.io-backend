4a3e79abe43dedc58d354a63bde13c83227d9495
Robin Härle <robin@blckct.io>
2021-05-24 18:31:30 +0200

    fix: make favorite work w/ other watch status values

--- consumatio/usecases/list.py
+++ consumatio/usecases/list.py
@@ -12,18 +12,23 @@ class List:
         # results = db.session.query().from_statement(
         #     text("SELECT * FROM media_data")).all()
 
-        query = "SELECT * FROM media_data , user_data WHERE user_data.user_id_content = media_data.user_id_content_media_data AND media_data.watch_status_content = :watch_status AND media_data.media_type_content = :type AND user_data.external_id_content = :user;"
+        query = "SELECT * FROM media_data , user_data WHERE user_data.user_id_content = media_data.user_id_content_media_data"
         results = []
 
         if watchStatus == "any" and favorite:
-            query = "SELECT * FROM media_data , user_data WHERE user_data.user_id_content = media_data.user_id_content_media_data AND media_data.favorite_content = :favorite AND media_data.media_type_content = :type AND user_data.external_id_content = :user;"
+            query += " AND media_data.favorite_content = :favorite AND media_data.media_type_content = :type AND user_data.external_id_content = :user;"
             results = MediaData.query.from_statement(text(query)).params(
                 favorite=favorite, type=type, user=user).all()
         elif watchStatus == "any":
-            query = "SELECT * FROM media_data , user_data WHERE user_data.user_id_content = media_data.user_id_content_media_data AND media_data.media_type_content = :type AND user_data.external_id_content = :user;"
+            query += " AND media_data.media_type_content = :type AND user_data.external_id_content = :user;"
             results = MediaData.query.from_statement(text(query)).params(
                 type=type, user=user).all()
+        elif favorite:
+            query += " AND media_data.watch_status_content = :watch_status AND media_data.favorite_content = :favorite AND media_data.media_type_content = :type AND user_data.external_id_content = :user;"
+            results = MediaData.query.from_statement(text(query)).params(
+                watch_status=watchStatus, favorite=favorite, type=type, user=user).all()
         else:
+            query += " AND media_data.watch_status_content = :watch_status AND media_data.media_type_content = :type AND user_data.external_id_content = :user;"
             results = MediaData.query.from_statement(text(query)).params(
                 watch_status=watchStatus, type=type, user=user).all()
 
