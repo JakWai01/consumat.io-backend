9883df737e5310c2dd1cf40f2945af2c618c43a8
dentremor <hillerdaniel11@gmail.com>
2021-05-27 17:55:06 +0200

    refactor: Remove episode and season from usecase rating

--- consumatio/usecases/rating.py
+++ consumatio/usecases/rating.py
@@ -1,15 +1,12 @@
 def set_rating(tmdb: object, database: object, external_id: str, media: str,
-               code: int, seasonNumber: int, episodeNumber: int,
-               rating: float) -> dict:
+               code: int, rating: int) -> dict:
     """
     :param tmdb: <object> TMDB object to make API requests
     :param database: <object> Database object to access the database
     :param external_id: <str> External ID provided by OAuth
     :param media: <str> Type of media to set rating for
     :param code: <int> Code of the media
-    :param seasonNumber: <int> Number of season to set rating for
-    :param episodeNumber: <int> Number of episode to set rating for
-    :param rating: <float> rating
+    :param rating: <int> rating
     :return: <dict> Successful response
     """
     user_id = 0
