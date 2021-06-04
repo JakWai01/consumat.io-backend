191460978591f8e20a046aef3583ec0f8fc266b9
JakWai01 <jakob.waibel@web.de>
2021-06-04 16:02:25 +0200

    refactor: Movie logic from search and popular into usecases

--- consumatio/gateways/popular_gateways/popular_movies_to_dict.py
+++ consumatio/gateways/popular_gateways/popular_movies_to_dict.py
@@ -1,35 +1,21 @@
-from consumatio.entities.movie import Movie
 from consumatio.external.db.models import *
-from sqlalchemy import text
 
 
-def popular_movies_to_dict(data: dict, external_id: str) -> dict:
+def popular_movies_to_dict(data: dict) -> dict:
     """
     Create dictionary for internal representation
     :param data: <dict> API response
     :return: <dict> Internal representation
     """
     result_list = []
-    result_dict = {"results": result_list, "total_pages": data["total_pages"]}
     if "results" not in data:
-        return result_dict
+        return result_list
     elif len(data["results"]) == 0:
-        return result_dict
+        return result_list
     else:
         results = data["results"]
         for result in results:
 
-            query = MediaData.query.join(User).filter(
-                User.user_id_content == MediaData.user_id_content_media_data,
-                MediaData.media_type_content == "Movie",
-                User.external_id_content == external_id,
-                MediaData.media_id_content == result.get("id")).first()
-
-            rating = None
-            watch_status = None
-            if query != None:
-                rating = query.rating_content
-                watch_status = query.watch_status_content
             dict = {
                 "code": result.get("id"),
                 "title": result.get("title"),
@@ -48,16 +34,8 @@ def popular_movies_to_dict(data: dict, external_id: str) -> dict:
                 "directors": None,
                 "tmdb_url":
                 f'https://www.themoviedb.org/movie/{result.get("id")}',
-                "watch_status": watch_status,
-                "rating_user": rating,
-                "favorite": None
             }
 
-            movie = Movie.from_dict(dict)
-
-            dict = movie.to_dict()
-
-            dict["__typename"] = "Movie"
-
             result_list.append(dict)
-        return result_dict
+
+        return {"total_pages": data["total_pages"], "results": result_list}
--- consumatio/gateways/search_gateways/search_result_to_dict.py
+++ consumatio/gateways/search_gateways/search_result_to_dict.py
@@ -11,29 +11,15 @@ def search_result_to_dict(data: dict, external_id: str) -> dict:
     :return: <dict> Internal representation
     """
     result_list = []
-    result_dict = {"results": result_list, "total_pages": data["total_pages"]}
     if "results" not in data:
-        return result_dict
+        return result_list
     elif len(data["results"]) == 0:
-        return result_dict
+        return result_list
     else:
         results = data["results"]
 
         for result in results:
             if result.get("media_type") == "tv":
-
-                query = MediaData.query.join(User).filter(
-                    User.user_id_content ==
-                    MediaData.user_id_content_media_data,
-                    MediaData.media_type_content == 'TV',
-                    User.external_id_content == external_id,
-                    MediaData.media_id_content == result.get("id")).first()
-
-                rating = None
-                watch_status = None
-                if query != None:
-                    rating = query.rating_content
-                    watch_status = query.watch_status_content
 
                 dict = {
                     "code": result.get("id"),
@@ -54,31 +40,11 @@ def search_result_to_dict(data: dict, external_id: str) -> dict:
                     "number_of_seasons": None,
                     "tmdb_url":
                     f'https://www.themoviedb.org/tv/{result.get("id")}',
-                    "watch_status": watch_status,
-                    "rating_user": rating,
-                    "favorite": None,
                     "runtime": None,
                     "rating_count": result.get("vote_count")
                 }
 
-                tv = TV.from_dict(dict)
-
-                dict = tv.to_dict()
-
-                dict["__typename"] = "TV"
             elif result.get("media_type") == "movie":
-                query = MediaData.query.join(User).filter(
-                    User.user_id_content ==
-                    MediaData.user_id_content_media_data,
-                    MediaData.media_type_content == 'Movie',
-                    User.external_id_content == external_id,
-                    MediaData.media_id_content == result.get("id")).first()
-
-                rating = None
-                watch_status = None
-                if query != None:
-                    rating = query.rating_content
-                    watch_status = query.watch_status_content
                 dict = {
                     "code": result.get("id"),
                     "title": result.get("title"),
@@ -96,18 +62,9 @@ def search_result_to_dict(data: dict, external_id: str) -> dict:
                     "directors": None,
                     "tmdb_url":
                     f'https://www.themoviedb.org/movie/{result.get("id")}',
-                    "watch_status": watch_status,
-                    "rating_user": rating,
-                    "favorite": None,
                     "rating_count": result.get("vote_count")
                 }
-
-                movie = Movie.from_dict(dict)
-
-                dict = movie.to_dict()
-
-                dict["__typename"] = "Movie"
             else:
                 continue
             result_list.append(dict)
-        return result_dict
+        return {"total_pages": data["total_pages"], "results": result_list}
