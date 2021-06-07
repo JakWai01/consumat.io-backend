7257ee0850b430d4c90a97a2c7c0a9989eefde03
JakWai01 <jakob.waibel@web.de>
2021-06-08 01:38:46 +0200

    feat: Add docstrings

--- consumatio/external/logger.py
+++ consumatio/external/logger.py
@@ -2,7 +2,10 @@ import logging
 
 
 def get_logger_instance(name='') -> object:
-   
+   """
+   Configures the logger for the backend and returns a instance
+   :return: <object> Logger instance
+   """
    log = logging.getLogger(name)
    log.setLevel(level=logging.DEBUG)
 
